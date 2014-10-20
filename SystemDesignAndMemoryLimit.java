6f11b1ce12ab4be1468155523a140bef40596a58
sammul <wuxia1989@gmail.com>
2014-10-19 21:04:16 -0700

    10/19

--- SystemDesignAndMemoryLimit.java
+++ SystemDesignAndMemoryLimit.java
@@ -14,5 +14,22 @@ public class SystemDesignAndMemoryLimit{
 		}
 	}
 
-	
+	public static void findMissingInt(String filename){
+		Scanner s = new Scanner(new FileReader(filename));
+		long size = Integer.MAX_VALUE-Integer.MIN_VALUE;
+		byte[] field = new byte[0xFFFFFFFF/8];
+		while(s.hasNextInt()){
+			long v = s.nextInt();
+			v = v-Integer.MIN_VALUE;
+			field[l/8] |= 1<<(l%8);
+		}
+		for(int i=0; i<field.length; i++){
+			for(int j=0; j<8; j++){
+				if(field[i] & (1<<j) == 0){
+					long l = i*8+j;
+					System.out.println(l+Integer.MIN_VALUE);
+				}
+			}
+		}
+	}
 }
